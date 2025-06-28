4f07b54f29277cc5c3d5389f2d4e6837fcd5353f
mwsohn <mwsohn@gmail.com>
2025-06-28 08:58:00 -0400

    Update AnalysisOfVariance.jl

--- src/AnalysisOfVariance.jl
+++ src/AnalysisOfVariance.jl
@@ -223,7 +223,7 @@ julia> aov.pvalue[3] # P-value for mpg3
 ```
 """
 function anova(_df::AbstractDataFrame, dep::Symbol, cat::Symbol)
-    isa(_df[:,cat], CategoricalArray) || throw(ArgumentError("`cat` must be a Categorical Array"))
+    # isa(_df[:,cat], CategoricalArray) || throw(ArgumentError("`cat` must be a Categorical Array"))
     df2 = dropmissing(_df[!,[dep, cat]])
     fm = @eval @formula($dep ~ 1 + $cat)
     mm = modelmatrix(fm, df2)
